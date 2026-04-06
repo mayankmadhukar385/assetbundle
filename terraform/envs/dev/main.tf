bc20413e872c9d280a7b71a8c3ed6daf6f0d6dfb
mayankmadhukar385 <mayank.madhukar@pwc.com>
2026-04-06 16:37:22 +0530

    maintfup1

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -1,21 +1,22 @@
-module "unity_catalog_baseline" {
-  source = "../../modules/unity_catalog_baseline"
-
-  catalog_name          = var.catalog_name
-  schema_name           = var.schema_name
-  environment           = var.environment
-  force_destroy_catalog = var.force_destroy_catalog
-}
-
 module "workspace_baseline" {
+  count = (
+    var.secret_scope_name != null &&
+    var.engineers_group_name != null &&
+    var.analysts_group_name != null &&
+    var.cluster_policy_name != null
+  ) ? 1 : 0
+
   source = "../../modules/databricks_workspace_baseline"
 
-  environment         = var.environment
-  secret_scope_name   = var.secret_scope_name
-  engineers_group_name = var.engineers_group_name
-  analysts_group_name  = var.analysts_group_name
-  cluster_policy_name = var.cluster_policy_name
+  providers = {
+    databricks = databricks
+  }
 
+  environment             = var.environment
+  secret_scope_name       = var.secret_scope_name
+  engineers_group_name    = var.engineers_group_name
+  analysts_group_name     = var.analysts_group_name
+  cluster_policy_name     = var.cluster_policy_name
   default_node_type_id    = var.default_node_type_id
   min_workers             = var.min_workers
   max_workers             = var.max_workers
